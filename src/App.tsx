ee6d58c1cded8e97f1b8d53318ddd046e5b7a20a
Ilamparithi-I <ilam.bissman@gmail.com>
2024-11-28 00:26:35 -0800

    Hopefully this fixes the routing issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,30 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from "react";
+import { Routes, Route, useLocation, useNavigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Project from "./pages/Project";
 import NotFound from "./pages/NotFound";
 
 function App() {
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    // On initial load, check for 'redirectTo' query parameter and navigate if present
+    React.useEffect(() => {
+        const params = new URLSearchParams(location.search);
+        const redirectTo = params.get("redirectTo");
+        if (redirectTo) {
+            navigate(redirectTo); // Navigate to the original path
+        }
+    }, [location, navigate]);
+
     return (
-        <Router>
-            <Routes>
-                <Route path="/">
-                    <Route index element={<Home />} />
-                    <Route path="projects/:proj" element={<Project />} />
-                    <Route path="*" element={<NotFound />} />
-                </Route>
-            </Routes>
-        </Router>
+        <Routes>
+            <Route path="/">
+                <Route index element={<Home />} />
+                <Route path="projects/:proj" element={<Project />} />
+                <Route path="*" element={<NotFound />} />
+            </Route>
+        </Routes>
     );
 }
 
